c9b7aa6a6aba934ac7e0e170a9fca9db9ddadf9b
tuglar <tulgardinc@gmail.com>
2025-05-26 22:16:06 +0200

    cube

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const zalg = @import("zalgebra");
 
 const sokol = @import("sokol");
 const sg = sokol.gfx;
@@ -6,12 +7,20 @@ const sapp = sokol.app;
 const slog = sokol.log;
 const sglue = sokol.glue;
 
-const shd = @import("shaders/compiled/triangle.glsl.zig");
+const shd = @import("shaders/compiled/cube.glsl.zig");
 
 const state = struct {
     var pass_action: sg.PassAction = .{};
     var bind: sg.Bindings = .{};
     var pip: sg.Pipeline = .{};
+    var rx: f32 = 0.0;
+    var ry: f32 = 0.0;
+
+    const view: zalg.Mat4 = zalg.Mat4.lookAt(
+        zalg.Vec3.new(0.0, 1.5, 6.0),
+        zalg.Vec3.zero(),
+        zalg.Vec3.up(),
+    );
 };
 
 export fn init() void {
@@ -69,17 +78,12 @@ export fn init() void {
         }),
     });
 
-    state.bind.index_buffer = sg.makeBuffer(.{
-        .usage = .{ .index_buffer = true },
-        .data = sg.asRange(&[_]u16{ 0, 1, 2, 2, 3, 0 }),
-    });
-
     state.pip = sg.makePipeline(.{
-        .shader = sg.makeShader(shd.triangleShaderDesc(sg.queryBackend())),
+        .shader = sg.makeShader(shd.cubeShaderDesc(sg.queryBackend())),
         .layout = init: {
             var l = sg.VertexLayoutState{};
-            l.attrs[shd.ATTR_triangle_position].format = .FLOAT3;
-            l.attrs[shd.ATTR_triangle_color0].format = .FLOAT4;
+            l.attrs[shd.ATTR_cube_position].format = .FLOAT3;
+            l.attrs[shd.ATTR_cube_color0].format = .FLOAT4;
             break :init l;
         },
         .index_type = .UINT16,
@@ -100,15 +104,32 @@ export fn init() void {
 }
 
 export fn frame() void {
+    const deltaTime: f32 = @floatCast(sapp.frameDuration() * 60);
+    state.rx += deltaTime;
+    state.ry += deltaTime * 2.0;
+    const vs_params = computeVsParams();
+
     sg.beginPass(.{
         .action = state.pass_action,
         .swapchain = sglue.swapchain(),
     });
     sg.applyPipeline(state.pip);
     sg.applyBindings(state.bind);
+    sg.applyUniforms(shd.UB_vs_params, sg.asRange(&vs_params));
     sg.draw(0, 36, 1);
     sg.endPass();
     sg.commit();
+}
+
+fn computeVsParams() shd.VsParams {
+    const rxm = zalg.Mat4.fromRotation(state.rx, zalg.Vec3.right());
+    const rym = zalg.Mat4.fromRotation(state.ry, zalg.Vec3.up());
+    const model = zalg.Mat4.mul(rxm, rym);
+    const aspect = sapp.widthf() / sapp.heightf();
+    const proj = zalg.Mat4.perspective(60, aspect, 0.01, 10.0);
+    return shd.VsParams{
+        .mvp = zalg.Mat4.mul(zalg.Mat4.mul(proj, state.view), model),
+    };
 }
 
 export fn cleanup() void {
@@ -126,5 +147,6 @@ pub fn main() !void {
         .window_title = "test",
         .logger = .{ .func = slog.func },
         .win32_console_attach = true,
+        .swap_interval = 1,
     });
 }
