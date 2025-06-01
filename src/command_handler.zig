ebfbca7b1859a8162fbe62ba353a03a466a72558
tuglar <tulgardinc@gmail.com>
2025-06-01 15:24:49 +0200

    support for modifiers

--- src/command_handler.zig
+++ src/command_handler.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 const Mode = @import("main.zig").Mode;
 const sapp = @import("sokol").app;
 
+const CodeWithMod = packed struct {
+    shift: bool = false,
+    control: bool = false,
+    alt: bool = false,
+    super: bool = false,
+    code_point: u28 = 0,
+};
+
 const ActionMap = std.AutoHashMap(u32, MapOrAction);
 
 const ActionMaps = struct {
@@ -68,24 +76,26 @@ pub fn addCommand(self: *Self, bind: []const u8, mode: Mode, action: *const fn (
     var char_index: usize = 0;
 
     while (char_index < bind.len) {
-        const char = bind[char_index];
+        const char: u32 = @intCast(bind[char_index]);
 
         // TODO: escape
         if (char == '<') {
             var end_index: usize = 0;
-            for (bind[char_index + 1 ..]) |i| {
-                if (char == '>') end_index = i;
+            for (char_index + 1..bind.len) |i| {
+                if (bind[i] == '>') end_index = i;
             }
             if (end_index == 0) return error.MalformedBind;
 
-            const key_code = parseKey(bind[char_index + 1 .. end_index - 1]) orelse return error.KeyNotFound;
+            const code_with_mod = try parseKey(bind[char_index + 1 .. end_index]);
+
+            std.debug.print("registered: {b}\n", .{code_with_mod});
 
             if (current_maps_ptr.key_map == null) {
                 current_maps_ptr.key_map = ActionMap.init(self.allocator);
             }
             const key_map_ptr = &current_maps_ptr.key_map.?;
 
-            const result = try key_map_ptr.getOrPut(key_code);
+            const result = try key_map_ptr.getOrPut(code_with_mod);
             if (!result.found_existing) {
                 if (end_index == bind.len - 1) {
                     result.value_ptr.* = MapOrAction{ .action = action };
@@ -106,14 +116,14 @@ pub fn addCommand(self: *Self, bind: []const u8, mode: Mode, action: *const fn (
         }
         const char_map_ptr = &current_maps_ptr.char_map.?;
 
-        const result = try char_map_ptr.getOrPut(char);
+        const char_shifted = char << 4;
+
+        const result = try char_map_ptr.getOrPut(char_shifted);
         if (!result.found_existing) {
             if (char_index == bind.len - 1) {
-                std.debug.print("suffix char {c}\n", .{char});
                 result.value_ptr.* = MapOrAction{ .action = action };
                 return;
             } else {
-                std.debug.print("prefix char {c}\n", .{char});
                 result.value_ptr.* = MapOrAction{ .maps = ActionMaps{} };
             }
         }
@@ -141,7 +151,7 @@ pub fn removeCommand(self: *Self, bind: []const u8, mode: Mode) !void {
             }
             if (end_index == 0) error.MalformedBind;
 
-            const key_code = parseKey(bind[char_index + 1 .. end_index - 1]) orelse return error.KeyNotFound;
+            const key_code = try parseKey(bind[char_index + 1 .. end_index - 1]);
 
             if (current_maps_ptr.key_map == null) return;
             const key_map_ptr = &current_maps_ptr.key_map.?;
@@ -189,10 +199,18 @@ pub fn onInput(self: *Self, mode: Mode, event: [*c]const sapp.Event) void {
 
         switch (e.*.type) {
             .KEY_DOWN => {
-                const key_code: u32 = @intCast(@intFromEnum(e.*.key_code));
+                const shifted_code: u32 = @bitCast(@intFromEnum(e.*.key_code) << 4);
+                const code_with_mod = shifted_code | e.*.modifiers;
+
+                if (e.*.char_code != 0) {
+                    std.debug.print("raw {b}\n", .{@intFromEnum(e.*.key_code)});
+                    std.debug.print("code {b}\n", .{shifted_code});
+                    std.debug.print("code + mod: {b}\n", .{code_with_mod});
+                }
+
                 if (current_maps_ptr.key_map == null) return;
                 const key_map_ptr = &current_maps_ptr.key_map.?;
-                const map_or_action_ptr = key_map_ptr.getPtr(key_code) orelse return;
+                const map_or_action_ptr = key_map_ptr.getPtr(code_with_mod) orelse return;
                 switch (map_or_action_ptr.*) {
                     .maps => |*maps_ptr| {
                         self.current_maps_ptr = maps_ptr;
@@ -205,11 +223,12 @@ pub fn onInput(self: *Self, mode: Mode, event: [*c]const sapp.Event) void {
                 }
             },
             .CHAR => {
-                const char_code = e.*.char_code;
-                std.debug.print("char: {c}\n", .{@as(u8, @intCast(char_code))});
+                const shifted_code: u32 = @bitCast(e.*.char_code << 4);
+                const code_with_mod = shifted_code;
+
                 if (current_maps_ptr.char_map == null) return;
                 const char_map_ptr = &current_maps_ptr.char_map.?;
-                const map_or_action_ptr = char_map_ptr.getPtr(char_code) orelse return;
+                const map_or_action_ptr = char_map_ptr.getPtr(code_with_mod) orelse return;
                 switch (map_or_action_ptr.*) {
                     .maps => |*maps_ptr| {
                         self.current_maps_ptr = maps_ptr;
@@ -230,7 +249,7 @@ pub fn clearInputSequence(self: *Self) void {
     self.current_map_ptr = null;
 }
 
-const parseKey: fn ([]const u8) ?u32 = blk: {
+const parseKey = blk: {
     @setEvalBranchQuota(5000);
     const keycode_enum_info: std.builtin.Type.Enum = @typeInfo(sapp.Keycode).@"enum";
 
@@ -250,8 +269,34 @@ const parseKey: fn ([]const u8) ?u32 = blk: {
     const static_map = std.static_string_map.StaticStringMap(u32).initComptime(kv_list);
 
     const func = struct {
-        pub fn parser(string: []const u8) ?u32 {
-            return static_map.get(string);
+        pub fn parser(string: []const u8) !u32 {
+            var i: usize = 0;
+            var input_start: usize = 0;
+
+            var code_with_mod = CodeWithMod{};
+
+            while (i < string.len) : (i += 1) {
+                const char = string[i];
+                if (char == '-') {
+                    switch (string[i - 1]) {
+                        'C' => code_with_mod.control = true,
+                        's' => code_with_mod.super = true,
+                        'S' => code_with_mod.shift = true,
+                        'A' => code_with_mod.alt = true,
+                        else => return error.MalformedBind,
+                    }
+                    if (i != string.len - 1) {
+                        input_start = i + 1;
+                    } else {
+                        return error.MalformedBind;
+                    }
+                }
+            }
+
+            std.debug.print("{s}\n", .{string[input_start .. i - 1]});
+            code_with_mod.code_point = @intCast(static_map.get(string[input_start..i]) orelse return error.InvalidKey);
+
+            return @bitCast(code_with_mod);
         }
     }.parser;
     @setEvalBranchQuota(1000);
--- src/commands.zig
+++ src/commands.zig
@@ -45,3 +45,67 @@ pub fn moveBottom() void {
         std.debug.print("{}\n", .{err});
     };
 }
+
+pub fn moveWordStartRight() void {
+    var found_first_gap = false;
+    for (State.buffer.gap_end..State.buffer.data.len) |i| {
+        if (State.buffer.data[i] == ' ' or State.buffer.data[i] == '\n') {
+            found_first_gap = true;
+        } else if (found_first_gap) {
+            State.buffer.moveGap(i - State.buffer.getGapLength()) catch |err| {
+                std.debug.print("{}\n", .{err});
+            };
+            return;
+        }
+    }
+    State.buffer.moveGap(State.buffer.data.len - State.buffer.getGapLength()) catch |err| {
+        std.debug.print("{}\n", .{err});
+    };
+}
+
+pub fn moveWordStartLeft() void {
+    if (State.buffer.gap_start == 0) return;
+
+    var i: usize = State.buffer.gap_start;
+    var found_first_word = State.buffer.data[i - 1] != ' ' and State.buffer.data[i - 1] != '\n';
+
+    while (i > 1) {
+        i -= 1;
+        if (State.buffer.data[i - 1] == ' ' or State.buffer.data[i - 1] == '\n') {
+            if (found_first_word) {
+                State.buffer.moveGap(i) catch |err| {
+                    std.debug.print("{}\n", .{err});
+                };
+                return;
+            }
+        } else {
+            found_first_word = true;
+        }
+    }
+    State.buffer.moveGap(0) catch |err| {
+        std.debug.print("{}\n", .{err});
+    };
+}
+
+pub fn moveWordEndRight() void {
+    if (State.buffer.gap_end == State.buffer.data.len) return;
+
+    const begin_char = State.buffer.data[State.buffer.gap_end + 1];
+    var found_first_word = begin_char != '\n' and begin_char != ' ';
+
+    for (State.buffer.gap_end..State.buffer.data.len - 1) |i| {
+        if (State.buffer.data[i + 1] == ' ' or State.buffer.data[i + 1] == '\n') {
+            if (found_first_word) {
+                State.buffer.moveGap(i - State.buffer.getGapLength()) catch |err| {
+                    std.debug.print("{}\n", .{err});
+                };
+                return;
+            }
+        } else {
+            found_first_word = true;
+        }
+    }
+    State.buffer.moveGap(State.buffer.data.len - State.buffer.getGapLength()) catch |err| {
+        std.debug.print("{}\n", .{err});
+    };
+}
